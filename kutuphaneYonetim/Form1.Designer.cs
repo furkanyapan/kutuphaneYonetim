f8295cc510de1e0a8f2cc85621abe06d7821c5b7
furkanyapan <furkanyapan@gmail.com>
2023-05-18 16:40:39 +0300

    Proje dosyası ekle.

---
+++ kutuphaneYonetim/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace kutuphaneYonetim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Aqua;
+            label1.Font = new Font("Showcard Gothic", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkViolet;
+            label1.Location = new Point(181, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(532, 31);
+            label1.TabIndex = 0;
+            label1.Text = "KUTUPHANE YONETIM PANELI GIRIS EKRANI";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(228, 518);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Kulanıcı Adı";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(276, 592);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Parola";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(377, 519);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(258, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(377, 593);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(258, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(521, 654);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 46);
+            button1.TabIndex = 5;
+            button1.Text = "Giriş Yap";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            linkLabel1.Location = new Point(377, 669);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(101, 20);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Kayıt Oluştur";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(314, 218);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(292, 220);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Aqua;
+            ClientSize = new Size(882, 853);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Giriş Yap";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ kutuphaneYonetim/Form2.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace kutuphaneYonetim
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Aqua;
+            label1.Font = new Font("Showcard Gothic", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkViolet;
+            label1.Location = new Point(186, 186);
+            label1.Name = "label1";
+            label1.Size = new Size(544, 31);
+            label1.TabIndex = 1;
+            label1.Text = "KUTUPHANE YONETIM PANELI KAYIT EKRANI";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(489, 550);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 46);
+            button1.TabIndex = 11;
+            button1.Text = "Kayıt Ol";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(345, 354);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(258, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(345, 278);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(258, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(191, 277);
+            label3.Name = "label3";
+            label3.Size = new Size(122, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(235, 496);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Üye No";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(260, 353);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 25);
+            label4.TabIndex = 12;
+            label4.Text = "Şifre";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(247, 420);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 25);
+            label5.TabIndex = 13;
+            label5.Text = "Görev";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(345, 497);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(258, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(345, 421);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(258, 27);
+            textBox4.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(345, 527);
+            label6.Name = "label6";
+            label6.Size = new Size(367, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Üye numaranızı veri tabanı yöneticinizden talep ediniz";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Aqua;
+            ClientSize = new Size(882, 853);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Kayıt Ol";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+    }
+}
